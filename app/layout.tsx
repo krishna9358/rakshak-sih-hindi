2400e9a4776b3abcde663e95bf2e13856c3fa9f0
Krishna Mohan <krishanmohank974@gmail.com>
2024-10-04 20:35:51 +0530

    removed pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -45,7 +45,7 @@ export default function HeroSection() {
             </div>
           </Link>
         </div>
-        <motion.div
+        <motion.div 
             initial={{ opacity: 0, y: -50 }} 
             animate={{ opacity: 1, y: 0 }}    
             transition={{ duration: 0.9 }}>
