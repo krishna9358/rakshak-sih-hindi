9dbc82252f6985c2b0ba73cb34077b31dc8cdcf6
Krishna Mohan <krishanmohank974@gmail.com>
2024-11-25 13:29:37 +0530

    Added routes, dummy data

---
+++ config/data/station-inventory.ts
@@ -0,0 +1,10 @@
+export const stationInventory = [
+    {
+        id: 1,
+        name: "Station 1",
+        location: "Location 1",
+        status: "Active",
+        lastUpdated: "2021-01-01"
+    },  
+
+]
