b315db15ff264c5c1594ed2bf3af94eacb3e7cc0
Krishna Mohan <krishanmohank974@gmail.com>
2024-11-26 02:48:14 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -54,11 +54,11 @@ const config: Config = {
   			sidebar: {
   				DEFAULT: 'hsl(var(--sidebar-background))',
   				foreground: 'hsl(var(--sidebar-foreground))',
-  				// primary: 'hsl(var(--sidebar-primary))',
+  				primary: 'hsl(var(--sidebar-primary))',
   				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
   				accent: 'hsl(var(--sidebar-accent))',
   				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
-  				// border: 'hsl(var(--sidebar-border))',
+  				border: 'hsl(var(--sidebar-border))',
   				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
